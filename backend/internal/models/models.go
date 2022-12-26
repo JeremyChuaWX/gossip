149289689f6bcb6cc8b345127600ea1290fee17f
Jeremy Chua <chua.jeremy.wx@gmail.com>
2022-12-26 09:15:28 +0800

    [backend] add JSON tags to Base model

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Base struct {
-	ID        uint `gorm:"primaryKey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint      `gorm:"primaryKey"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type User struct {
