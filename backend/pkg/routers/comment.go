48ceaf9ec4c02c3ecedd1b48fab4218f0e214aba
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-03 15:46:14 +0800

    [backend] edit comment routes

--- backend/pkg/routers/comment.go
+++ backend/pkg/routers/comment.go
@@ -12,7 +12,7 @@ func initCommentRouter(router fiber.Router, DB *gorm.DB) {
 	cmtHandler := handlers.CommentHandler{DB: DB}
 	cmtRouter := router.Group("/comments")
 
-	cmtRouter.Post("/create-post", middlewares.Jwtware(), cmtHandler.CreateComment)
+	cmtRouter.Post("/create-comment", middlewares.Jwtware(), cmtHandler.CreateComment)
 	cmtRouter.Get("/get-comment/:id", cmtHandler.GetCommentById)
 	cmtRouter.Put("/update-comment/:id", middlewares.Jwtware(), cmtHandler.UpdateComment)
 	cmtRouter.Delete("/delete-comment/:id", middlewares.Jwtware(), cmtHandler.DeleteComment)
