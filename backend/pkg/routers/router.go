77a749fdfd03087d3ba9dd434cba0c08064395bd
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-04 18:15:37 +0800

    [backend] add cors middleware

--- backend/pkg/routers/router.go
+++ backend/pkg/routers/router.go
@@ -2,12 +2,18 @@ package routers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"gorm.io/gorm"
 )
 
 func Initialise(app *fiber.App, DB *gorm.DB) {
-	api := app.Group("/api", logger.New())
+	api := app.Group("/api")
+
+	api.Use(logger.New())
+	api.Use(cors.New(cors.Config{
+		AllowOrigins: "localhost:3000",
+	}))
 
 	initAuthRouter(api, DB)
 	initUserRouter(api, DB)
