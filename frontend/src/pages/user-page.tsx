48b007948fef75672479098bae7e8fd93ba2fd0a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-08 17:58:25 +0800

    [frontend] add tailwind

--- frontend/src/pages/user-page.tsx
+++ frontend/src/pages/user-page.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
-import { getUserQuery } from "../loaders/users";
+import { getUserQuery } from "../utils/react-query/user";
 
 export default function UserPage() {
   const params: any = useParams();
