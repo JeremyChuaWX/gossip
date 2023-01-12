38cb4e22d0edaf1e1ddc680f631780ad8939d85f
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-12 13:45:18 +0800

    [frontend] posts api calls

--- frontend/src/api/posts.ts
+++ frontend/src/api/posts.ts
@@ -21,5 +21,41 @@ async function getPost(input: GetPostInput) {
   return res.data;
 }
 
-export type { GetPostInput };
-export { getPost, getPosts };
+interface UpdatePostInput {
+  id: string;
+  title?: string;
+  body?: string;
+  post_score?: number;
+}
+
+async function updatePost(input: UpdatePostInput) {
+  const res = (
+    await axiosConfig.put<ServerResponse<Post>>(
+      `posts/update-post/${input.id}`,
+      {
+        title: input.title,
+        body: input.body,
+        post_score: input.post_score,
+      }
+    )
+  ).data;
+
+  return res.data;
+}
+
+interface DeletePostInput {
+  id: string;
+}
+
+async function deletePost(input: DeletePostInput) {
+  const res = (
+    await axiosConfig.delete<ServerResponse<Post>>(
+      `posts/delete-post${input.id}`
+    )
+  ).data;
+
+  return res.data;
+}
+
+export type { GetPostInput, UpdatePostInput, DeletePostInput };
+export { getPost, getPosts, updatePost, deletePost };
