03cdc8dbd70241a471dd265c6d9e78dbe983c853
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-14 11:22:15 +0800

    [frontend] extract navbar component

--- frontend/src/layouts/base-layout.tsx
+++ frontend/src/layouts/base-layout.tsx
@@ -1,64 +1,12 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getMeQuery } from "../api/users/queries";
-import { useSignOutMutation } from "../api/auth/mutations";
-
-function NavButton({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="duration-75 ease-out hover:scale-110 group">
-      {children}
-      <span className="block max-w-0 h-0.5 bg-black transition-all duration-75 group-hover:max-w-full" />
-    </div>
-  );
-}
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/nav-bar";
 
 function BaseLayout() {
-  const navigate = useNavigate();
-
-  const { mutate: signOut } = useSignOutMutation();
-  const { data: user } = useQuery(getMeQuery());
-
-  const signOutOnClick = () => {
-    signOut(undefined, {
-      onSuccess: () => {
-        navigate("/");
-      },
-    });
-  };
-
   return (
     <div className="w-full h-full">
-      <header className="flex justify-between items-center p-4">
-        <NavButton>
-          <NavLink className="text-2xl font-bold" to="/">
-            Gossip
-          </NavLink>
-        </NavButton>
-        <nav className="flex gap-4">
-          {user ? (
-            <>
-              <NavButton>
-                <button onClick={() => signOutOnClick()}>Sign Out</button>
-              </NavButton>
-              <NavButton>
-                <NavLink to="/profile">{user.username}</NavLink>
-              </NavButton>
-            </>
-          ) : (
-            <>
-              <NavButton>
-                <NavLink to="/auth/signin">Sign In</NavLink>
-              </NavButton>
-              <NavButton>
-                <NavLink to="/auth/signup">Sign Up</NavLink>
-              </NavButton>
-            </>
-          )}
-        </nav>
-      </header>
+      <NavBar />
 
-      <main className="px-4 pt-4 w-full h-full">
+      <main className="w-full h-full">
         <Outlet />
       </main>
     </div>
