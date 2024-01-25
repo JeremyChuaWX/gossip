f247711da7352a4d440ccec5292047f87faf6d0d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-01-25 14:21:02 +0800

    feat: CORS middleware

--- backend/router.go
+++ backend/router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type Router struct {
@@ -13,12 +14,14 @@ func NewRouter(h *Handlers) *Router {
 	mux := chi.NewRouter()
 
 	// middleware
+	mux.Use(cors.Handler(cors.Options{}))
 	mux.Use(middleware.StripSlashes)
 	mux.Use(middleware.CleanPath)
 	mux.Use(middleware.Logger)
 
 	// handlers
 	mux.Post("/room", h.NewRoomHandler)
+	mux.Get("/room", h.GetRoomsHandler)
 	mux.HandleFunc("/room/{room}", h.JoinRoomHandler)
 
 	return &Router{Mux: mux}
