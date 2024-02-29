56194c7ea2d55957ae447caf6ce85043ce12a58d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-02-29 23:20:03 +0800

    feat(webapp): signup form

---
+++ webapp/src/routes/signup/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+    import SignupForm from "./signup-form.svelte";
+
+    export let data: PageData;
+</script>
+
+<div class="container mt-[-3rem] flex h-full w-full flex-col items-center justify-center gap-8">
+    <h1 class="text-5xl font-extrabold">Gossip</h1>
+    <SignupForm data={data.form} />
+</div>
