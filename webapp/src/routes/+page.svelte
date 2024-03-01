fa18f9eb0c8afde8d8d27201608562300ecd24d8
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-03-01 08:23:31 +0800

    feat(webapp): use ease out in landing page animation

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div class="mt-[-3rem] flex h-full w-full flex-col items-center justify-center gap-16">
     <div class="group flex flex-col items-center gap-4">
         <h1 class="text-9xl font-extrabold">Gossip</h1>
-        <p class="animate-[widen-tracking_1s_ease-in-out_forwards] text-5xl">Speak your mind.</p>
+        <p class="animate-[widen-tracking_2s_ease-out_forwards] text-5xl">Speak your mind.</p>
     </div>
     <div class="flex w-fit gap-8">
         <Button href="/signup" variant="ghost" class="h-fit w-fit px-6 py-4 text-3xl"
