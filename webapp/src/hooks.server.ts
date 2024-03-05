d9f9d4aab5340638a4e6dad917bb77dba0a75ea0
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-03-05 21:18:19 +0800

    fix(webapp): only check sessionId and get user when in (authed) routes

--- webapp/src/hooks.server.ts
+++ webapp/src/hooks.server.ts
@@ -7,16 +7,16 @@ export const handle: Handle = async ({ event, resolve }) => {
     const sessionId = event.cookies.get(SESSION_ID_COOKIE);
 
     // https://github.com/sharu725/sveltekit-walkthrough-website/blob/master/src/hooks.server.js
-    if (sessionId === undefined && event.route.id?.includes("(authed)")) {
-        throw redirect(302, "/signin");
-    }
-
-    if (sessionId !== undefined) {
-        const res = await getCurrentUser({ sessionId });
-        if (res === undefined) {
+    if (event.route.id?.includes("(authed)")) {
+        if (sessionId === undefined) {
             throw redirect(302, "/signin");
+        } else {
+            const res = await getCurrentUser({ sessionId });
+            if (res === undefined) {
+                throw redirect(302, "/signin");
+            }
+            event.locals.user = res.user;
         }
-        event.locals.user = res.user;
     }
 
     return await resolve(event);
