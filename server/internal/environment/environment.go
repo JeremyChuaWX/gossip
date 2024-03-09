d078fcbe7828c6b044025664586761399935739a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-03-10 00:00:06 +0800

    fix(server): update environment variables

--- server/internal/environment/environment.go
+++ server/internal/environment/environment.go
@@ -1,16 +1,14 @@
 package environment
 
 import (
-	"errors"
+	"fmt"
 	"os"
 	"reflect"
 )
 
-var missingEnvError = errors.New("missing environment variable")
-
 type environment struct {
-	PostgresURL   string `env:"POSTGRES_URL"`
-	RedisURL      string `env:"REDIS_URL"`
+	PostgresURL   string `env:"SERVER_POSTGRES_URL"`
+	RedisURL      string `env:"SERVER_REDIS_URL"`
 	ServerAddress string `env:"SERVER_ADDRESS"`
 }
 
@@ -22,9 +20,13 @@ func Init() (environment, error) {
 		key := field.Tag.Get("env")
 		value := os.Getenv(key)
 		if value == "" {
-			return env, missingEnvError
+			return env, missingEnvError(key)
 		}
 		queryStructValue.FieldByIndex(field.Index).SetString(value)
 	}
 	return env, nil
+}
+
+func missingEnvError(key string) error {
+	return fmt.Errorf("missing environment variable: %s", key)
 }
