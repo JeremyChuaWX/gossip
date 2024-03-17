c6f14e644877eec911e57d7c9184b47849e7e83d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-03-17 21:47:49 +0800

    feat(webapp): scope out home page

--- webapp/src/routes/(nav)/(authed)/home/+page.svelte
+++ webapp/src/routes/(nav)/(authed)/home/+page.svelte
@@ -4,7 +4,19 @@
     export let data: PageData;
 </script>
 
-<div>
-    <h1>Home Page</h1>
-    <p>Welcome {data.user.username}!</p>
+<div class="mx-auto flex h-full w-[min(80%,1000px)]">
+    <div class="w-1/3 border-r border-zinc-800 p-4">
+        <div class="flex justify-between">
+            <h1>Rooms</h1>
+            <button>+</button>
+        </div>
+    </div>
+    <div class="flex h-full w-full flex-col border-r border-zinc-800">
+        <div class="h-full w-full p-4">
+            <p>Welcome {data.user.username}!</p>
+        </div>
+        <div class="flex h-12 w-full border-t border-zinc-800">
+            <input class="h-full w-full bg-transparent p-4" />
+        </div>
+    </div>
 </div>
