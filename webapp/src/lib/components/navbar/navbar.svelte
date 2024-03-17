9a7fac768c43ce87f9b1f13dfe8decd3cc560641
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-03-17 21:47:19 +0800

    feat(webapp): add border on navbar

--- webapp/src/lib/components/navbar/navbar.svelte
+++ webapp/src/lib/components/navbar/navbar.svelte
@@ -1,3 +1,3 @@
-<div class="flex w-full items-center px-8 py-4">
+<div class="flex w-full items-center border-b border-zinc-800 px-8 py-4">
     <a class="text-2xl font-extrabold" href="/">Gossip</a>
 </div>
