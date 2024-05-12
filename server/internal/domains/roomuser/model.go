4fc974dba66e0d155069246fbe2228b16a7142b8
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-05-12 21:34:20 +0800

    refactor: restructure

--- server/internal/domains/roomuser/model.go
+++
@@ -1,8 +0,0 @@
-package roomuser
-
-import "github.com/gofrs/uuid/v5"
-
-type RoomUser struct {
-	RoomId uuid.UUID `db:"room_id" json:"roomId"`
-	UserId uuid.UUID `db:"user_id" json:"userId"`
-}
--- server/internal/utils/urlquery/urlquery.go
+++ server/internal/utils/urlquery/urlquery.go
@@ -1,4 +1,4 @@
-package utils
+package urlquery
 
 import (
 	"errors"
