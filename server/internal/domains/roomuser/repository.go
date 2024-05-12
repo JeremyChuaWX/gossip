31d625b471de50e5635839e8966041d23a7085e2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-05-12 08:10:33 +0800

    feat: roomuser repository return slices

--- server/internal/domains/roomuser/repository.go
+++ server/internal/domains/roomuser/repository.go
@@ -2,6 +2,8 @@ package roomuser
 
 import (
 	"context"
+	"gossip/internal/domains/room"
+	"gossip/internal/domains/user"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -19,10 +21,12 @@ func (r *Repository) Create(
 	INSERT INTO room_users (
 		room_id,
 		user_id
-	) VALUES (
+	)
+	VALUES (
 		$1,
 		$2
-	) RETURNING
+	)
+	RETURNING
 		room_id,
 		user_id
 	;
@@ -31,44 +35,13 @@ func (r *Repository) Create(
 	return pgx.CollectExactlyOneRow[RoomUser](rows, pgx.RowToStructByName)
 }
 
-func (r *Repository) FindManyByRoomId(
-	ctx context.Context,
-	dto FindManyByRoomIdDTO,
-) (RoomUser, error) {
-	sql := `
-	SELECT
-		room_id,
-		user_id
-	FROM room_users WHERE
-		room_id = $1
-	;
-	`
-	rows, _ := r.PgPool.Query(ctx, sql, dto.RoomId)
-	return pgx.CollectExactlyOneRow[RoomUser](rows, pgx.RowToStructByName)
-}
-
-func (r *Repository) FindManyByUserId(
-	ctx context.Context,
-	dto FindManyByUserIdDTO,
-) (RoomUser, error) {
-	sql := `
-	SELECT
-		room_id,
-		user_id
-	FROM room_users WHERE
-		user_id = $1
-	;
-	`
-	rows, _ := r.PgPool.Query(ctx, sql, dto.UserId)
-	return pgx.CollectExactlyOneRow[RoomUser](rows, pgx.RowToStructByName)
-}
-
 func (r *Repository) Delete(
 	ctx context.Context,
 	dto DeleteDTO,
 ) (RoomUser, error) {
 	sql := `
-	DELETE FROM room_users WHERE
+	DELETE FROM room_users
+	WHERE
 		room_id = $1,
 		user_id = $2
 	RETURNING
@@ -78,4 +51,41 @@ func (r *Repository) Delete(
 	`
 	rows, _ := r.PgPool.Query(ctx, sql, dto.RoomId, dto.UserId)
 	return pgx.CollectExactlyOneRow[RoomUser](rows, pgx.RowToStructByName)
+}
+
+func (r *Repository) FindManyUsersByRoomId(
+	ctx context.Context,
+	dto FindManyByRoomIdDTO,
+) ([]user.User, error) {
+	sql := `
+	SELECT
+		users.id,
+		users.username,
+		users.password_hash
+	FROM room_users
+		INNER JOIN users ON users.id = room_users.user_id
+	WHERE
+		room_users.room_id = $1
+	;
+	`
+	rows, _ := r.PgPool.Query(ctx, sql, dto.RoomId)
+	return pgx.CollectRows[user.User](rows, pgx.RowToStructByName)
+}
+
+func (r *Repository) FindManyRoomsByUserId(
+	ctx context.Context,
+	dto FindManyByUserIdDTO,
+) ([]room.Room, error) {
+	sql := `
+	SELECT
+		rooms.id,
+		rooms.name
+	FROM room_users
+		INNER JOIN rooms ON rooms.id = room_users.room_id
+	WHERE
+		room_users.user_id = $1
+	;
+	`
+	rows, _ := r.PgPool.Query(ctx, sql, dto.UserId)
+	return pgx.CollectRows[room.Room](rows, pgx.RowToStructByName)
 }
