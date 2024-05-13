147958b7183460648126dcfa300b8293ae93b473
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-05-13 09:23:07 +0800

    feat: constants

---
+++ server/internal/api/constants.go
@@ -0,0 +1,7 @@
+package api
+
+type UserContextKey string
+
+const USER_ID_CONTEXT_KEY UserContextKey = "USER_ID"
+
+const SESSION_ID_HEADER = "x-session-id"
--- server/internal/services/session/constants.go
+++ server/internal/services/session/constants.go
@@ -2,10 +2,4 @@ package session
 
 import "time"
 
-type UserContextKey string
-
-const (
-	USER_ID_CONTEXT_KEY UserContextKey = "USER_ID"
-	SESSION_ID_HEADER                  = "x-session-id"
-	SESSION_EXPIRATION                 = time.Hour * 24 * 7
-)
+const SESSION_EXPIRATION = time.Hour * 24 * 7
