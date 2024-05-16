d3fbfe5ad3af411b6ae62aa4b0f5addade44065a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-05-16 08:57:14 +0800

    feat: remove unused events

--- server/internal/services/chat/event.go
+++ server/internal/services/chat/event.go
@@ -5,12 +5,6 @@ import "github.com/gofrs/uuid/v5"
 type eventName string
 
 const (
-	USER_CONNECT    eventName = "USER_CONNECT"
-	USER_DISCONNECT eventName = "USER_DISCONNECT"
-
-	ROOM_CREATE  eventName = "ROOM_CREATE"
-	ROOM_DESTROY eventName = "ROOM_DESTROY"
-
 	MESSAGE         eventName = "MESSAGE"
 	USER_JOIN_ROOM  eventName = "USER_JOIN_ROOM"
 	USER_LEAVE_ROOM eventName = "USER_LEAVE_ROOM"
