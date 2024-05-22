f8247c5def6767140c7246d266df51202ef4babe
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-05-22 10:01:46 +0800

    feat: middleware

--- server/internal/api/middlewares/middlewares.go
+++ server/internal/api/middlewares/middlewares.go
@@ -3,18 +3,16 @@ package middlewares
 import (
 	"context"
 	"errors"
-	"gossip/internal/constants"
-	"gossip/internal/domains/session"
-	"gossip/internal/utils"
+	"gossip/internal/api"
+	"gossip/internal/services/session"
+	"gossip/internal/utils/httpjson"
 	"net/http"
-
-	"github.com/gofrs/uuid/v5"
 )
 
 var invalidSessionIdError = errors.New("invalid session ID")
 
 type Middlewares struct {
-	SessionRepository *session.Repository
+	SessionService *session.Service
 }
 
 type middleware func(http.Handler) http.Handler
@@ -22,36 +20,27 @@ type middleware func(http.Handler) http.Handler
 func (m *Middlewares) AuthMiddleware() middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			sessionId := r.Header.Get(constants.SESSION_ID_HEADER)
+			sessionId := r.Header.Get(api.SESSION_ID_HEADER)
 			if sessionId == "" {
-				utils.WriteError(
+				httpjson.WriteError(
 					w,
 					http.StatusUnauthorized,
 					invalidSessionIdError,
 				)
 				return
 			}
-
-			userIdStr, err := m.SessionRepository.Get(r.Context(), sessionId)
+			userId, err := m.SessionService.Get(r.Context(), sessionId)
 			if err != nil {
-				utils.WriteError(w, http.StatusUnauthorized, err)
+				httpjson.WriteError(w, http.StatusUnauthorized, err)
 				return
 			}
-
-			userId, err := uuid.FromString(userIdStr)
-			if err != nil {
-				utils.WriteError(w, http.StatusUnauthorized, err)
-				return
-			}
-
 			nextReq := r.WithContext(
 				context.WithValue(
 					r.Context(),
-					constants.USER_ID_CONTEXT_KEY,
+					api.USER_ID_CONTEXT_KEY,
 					userId,
 				),
 			)
-
 			next.ServeHTTP(w, nextReq)
 		})
 	}
