2d06dd1cbca36c7973dabc8fc0d547d3e7b741ce
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-08-04 23:21:07 +0800

    style: format

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.html",
-        "./static/js/**/*.js",
-    ],
+    content: ["./pages/**/*.html", "./static/js/**/*.js"],
     theme: {
         extend: {},
     },
